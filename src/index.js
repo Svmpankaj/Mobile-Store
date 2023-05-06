a0442ed7cb04225c5a0f6836045b365a9b067321
Svmpankaj <svmpankaj@gmail.com>
2023-05-06 23:55:59 +0530

    cartSlice is created

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { store } from './store';
+import { Provider } from 'react';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
